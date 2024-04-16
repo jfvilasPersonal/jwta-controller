6c0410dd2fb8b2183831a52321c17888f52b0c50
Julio Fernandez <jfvilas@outlook.com>
2024-04-16 18:44:57 +0200

    BasicAuthList

---
+++ publish-controller-to-k3d.cmd
@@ -0,0 +1,13 @@
+call npm run build
+kubectl delete -f crd\crd.yaml
+kubectl delete -f controller.yaml
+kubectl delete configmap obk-authorizator-ingress-jfvilas-configmap -n dev
+kubectl delete deployment obk-authorizator-ingress-jfvilas-deply -n dev
+kubectl delete service  obk-authorizator-ingress-jfvilas-svc -n dev
+docker image rm obk-controller:latest
+set DOCKER_BUILDKIT=1
+set COMPOSE_DOCKER_CLI_BUILD=0
+docker build . -t obk-controller:latest
+k3d image import obk-controller:latest -t -c oberkorn
+kubectl apply -f crd\crd.yaml
+kubectl apply -f controller.yaml
