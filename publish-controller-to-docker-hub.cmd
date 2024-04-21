56f1386d64808e73f96e1cb210ca3018d45c6ca1
Julio Fernandez <jfvilas@outlook.com>
2024-04-21 19:57:43 +0200

    cl

--- publish-controller-to-docker-hub.cmd
+++ publish-controller-to-docker-hub.cmd
@@ -2,7 +2,7 @@ call npm run build
 set /p major=<major
 set /p minor=<minor
 set /p level=<level
-set currversion=%major%.%minor%.%level%
+set currentversion=%major%.%minor%.%level%
 docker image rm obk-controller:latest
 docker build . -t obk-controller -t jfvilasoutlook/obk-controller:%currentversion% -t jfvilasoutlook/obk-controller:latest
 docker push jfvilasoutlook/obk-controller:%currentversion%
